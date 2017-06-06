81b688f34bf3a7806ff4462608b673abf9c735a1
SkBlaz <skrljblaz@gmail.com>
2017-06-06 18:37:52 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+ from setuptools import setup
+ 
+setup(
+    name='py3plex',    # This is the name of your PyPI-package.
+    version='0.3',                          # Update the version number for new releases
+    description="Multiplex network visualization library",
+    url="https://github.com/SkBlaz/supertest",
+    author="Blaz Skrlj",
+    author_email="blaz.skrlj@ijs.si",
+    license="MIT",
+    packages=['py3plex']
+)
