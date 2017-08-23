4f1c2db54e5e3de058507920e6c6bce89426e11d
SkBlaz <skrljblaz@gmail.com>
2017-08-23 20:48:36 +0200

    Algorithms basics

--- py3plex/algorithms.py
+++ py3plex/algorithms.py
@@ -1,26 +1,39 @@
 ## this file contains some of the multilayer network analysis network
 ## algorithms, used in the BioGrid analysis
 
-def compute_network_avp(multilayer_network):
+class multiplex_network:
 
-    ## for network in multilayer, compute intra network mean shortest paths
-    ## use sum (inter_network path) to sum the overall mean_path
-    ## can be made run in parallel!
-    ## this is used for normalization
-        
-    pass
+    def __init__(self,networks,edges,name):
+        self.networks = networks
+        self.edges = edges
+        self.name = name
 
+    def connectivity_ratio(self):
 
-def compare_by_avp(multilayer_network,sample_size,node_subsets):
+        ## compute ratio of inter_edges/intra_edges
+        pass
 
-    ## connected components
-    ## generate sample in parallel
-    ## output vector of multilayer sample avps
-    ## compare with node_subset avp data
-    ## perform some test to assess whether data comes from this distribution
+    def layerwise_modularity(self):
 
-    pass
+        ## how modular are different networks
+        pass
 
-## additional suggestions:
-## avp of nodeset is summed overall
+    def inter_layer_connections(self):
 
+        ## number of inter-layer connections
+        pass
+
+    def multiplex_entropy(self):
+
+        ## 
+        pass
+
+    def clustering_coefficient(self):
+
+        ##
+        pass
+
+    def transitivity(self):
+
+        ##
+        pass
