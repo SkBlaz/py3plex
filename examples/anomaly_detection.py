27e647a940add73ca138260b57fb935674747e7b
SkBlaz <skrljblaz@gmail.com>
2017-09-03 20:08:21 +0200

    Added anomaly counter

---
+++ examples/anomaly_detection.py
@@ -0,0 +1,76 @@
+## Author: Blaz Skrlj of IJS. This is the code for multiplex anomaly detection algorithm.
+
+from py3plex.algorithms import *
+from collections import defaultdict
+import pandas as pd ## this is for tabelaric manipulations
+
+networks = defaultdict(list)
+label_dict = {}
+
+
+path_edges = "../testgraph/multiplex_datasets/NYClimateMarch2014_Multiplex_Social/Dataset/NYClimateMarch2014_multiplex.edges"
+
+path_labels = "../testgraph/multiplex_datasets/NYClimateMarch2014_Multiplex_Social/Dataset/NYClimateMarch2014_layers.txt"
+
+path_activity = "../testgraph/multiplex_datasets/NYClimateMarch2014_Multiplex_Social/Dataset/NYClimateMarch2014_activity.txt"
+
+with open(path_edges) as me:
+    for line in me:
+        layer, n1, n2, weight = line.strip().split()
+        networks[layer].append((n1,n2))
+
+## get the labels
+with open(path_labels) as lx:
+    for line in lx:
+        lid, lname = line.strip().split()
+        label_dict[lid] = lname
+
+multilayer_network = []
+labs = []
+
+for network_id, network_data in networks.items():
+    G = nx.Graph()
+    G.add_edges_from(network_data)
+    print(nx.info(G))
+    multilayer_network.append(G)
+    labs.append(label_dict[network_id])
+
+print("Found layers:",labs)
+
+## construct the mx object
+multi_object = multiplex_network(multilayer_network,[None],labs)
+multi_object.detect_communities()
+
+comcounts = pd.DataFrame(columns=['time','count','layer'])
+
+## iterate through the time-series
+tmplc = 0
+num_lines = sum(1 for line in open(path_activity))
+
+print("Starting the temporal component analysis..")
+with open(path_activity) as ax:
+    for line in ax:
+        tmplc +=1
+        if tmplc % 1000 == 0:
+            print("Progress:",str(tmplc*100/num_lines),"%")
+        try:
+            n1,n2,time,layer = line.strip().split()
+            tmpCount = 0
+            for com in multi_object.communities[layer]:
+                if n1 in com and n2 in com:
+                    tmpCount +=1
+
+            comcounts = comcounts.append({'time':time,'count':tmpCount,'layer' : layer})
+            
+        except:
+            ## those are beginning lines
+            pass
+
+print(comcounts.describe())
+print("Finished analysis..")            
+
+## run separate anomaly detection on time vectors
+## if >2;anomaly!
+
+
+
--- py3plex/algorithms.py
+++ py3plex/algorithms.py
@@ -1,17 +1,24 @@
 ## this file contains some of the multilayer network analysis network
 ## algorithms, used in the BioGrid analysis
-import networkx as nx
+
+import networkx as nx ## graph primitives
 from collections import defaultdict
-import numpy as np
-import itertools
+import numpy as np ## Lalgebra
+import itertools ## combinations
 
 class multiplex_network:
 
-    def __init__(self,networks,edges,name):
+    def __init__(self,networks,edges,name,timescale=None,verbose=True):
         self.networks = networks
         self.multiedges = edges
-        self.name = name
+        self.names = name
+        self.verbose = verbose
+        self.timescale = timescale
 
+    def monitor(self,message):
+        if self.verbose == True:
+            print(message)
+            
     def print_basic_info(self):
         print("MULTIPLEX OBJECT DESCRIPTION")
         print("Number of layers",len(self.networks))
@@ -80,12 +87,16 @@ class multiplex_network:
 
         return {'variability' : total_variability,'deviation' : total_deviance, 'mean' : total_mean,'data' : communities}
 
-            
-            
-            
+    def detect_communities(self):
+        communities = {}
+        import community
+        for network,name in zip(self.networks,self.names):
+            self.monitor("Finding communities for layer: "+name)
+            best_partition = community.best_partition(network)
+            dx = defaultdict(list)
+            for k,v in best_partition.items():
+                dx[v].append(k)
 
-    
-    def multiplex_degree_gain(self):
-
-        ## for each multiplex edge, check its degree and compare to layer mean degree
-        pass
+            communities[name] = list(dx.values())
+        self.monitor("Assigning communities..")
+        self.communities = communities
