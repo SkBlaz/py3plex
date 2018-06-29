4345010510d264fc2796ef9b72c0f52b6b3e29fd
SkBlaz <skrljblaz@gmail.com>
2018-06-29 22:07:23 +0200

    added minor data change

--- examples/example_multilayer_visualization.py
+++ examples/example_multilayer_visualization.py
@@ -1,19 +1,19 @@
 ## visualization of a simple heterogeneous network
 from py3plex.visualization.multilayer import *
-from py3plex.visualization.colors import all_color_names
+from py3plex.visualization.colors import all_color_names,colors_default
 from py3plex.core import multinet
 
 ## multilayer
-multilayer_network = multinet.multi_layer_network().load_network("../datasets/epigenetics.gpickle",directed=False, input_type="gpickle_biomine")
+multilayer_network = multinet.multi_layer_network().load_network("../datasets/goslim_mirna.gpickle",directed=False, input_type="gpickle_biomine")
 multilayer_network.basic_stats() ## check core imports
 network_labels, graphs, multilinks = multilayer_network.get_layers() ## get layers for visualization
 #print(network_labels,graphs)
 draw_multilayer_default(graphs,display=False,background_shape="circle",labels=network_labels)
 
 enum = 1
-color_mappings = {idx : col for idx, col in enumerate(list(all_color_names.keys()))}
+color_mappings = {idx : col for idx, col in enumerate(colors_default)}
 for edge_type,edges in multilinks.items():
-    draw_multiedges(graphs,edges,alphachannel=0.7,linepoints="-.",linecolor=color_mappings[enum],curve_height=5,linmod="upper",linewidth=0.4)
+    draw_multiedges(graphs,edges,alphachannel=0.2,linepoints="-.",linecolor=color_mappings[enum],curve_height=5,linmod="upper",linewidth=0.4)
     enum+=1
 plt.show()
 
