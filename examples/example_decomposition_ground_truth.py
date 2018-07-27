af90fc576425a6cceb37fa8e330f2e9a8bc357ac
skblaz <skrljblaz@gmail.com>
2018-07-27 13:28:47 +0200

    added description of decomposition example

--- examples/example_decomposition_ground_truth.py
+++ examples/example_decomposition_ground_truth.py
@@ -3,6 +3,8 @@
 from py3plex.core import multinet
 from py3plex.algorithms.network_ranking.node_ranking import sparse_page_rank, stochastic_normalization_hin
 
+## a simple decomposition example. Note that target nodes need to have "labels" property, to which labels are assigned in class1---class2---...and so on...
+
 dataset = "../datasets/labeled_epigenetics.gpickle"
 
 multilayer_network = multinet.multi_layer_network().load_network(input_file=dataset,directed=True,input_type=dataset.split(".")[-1])
