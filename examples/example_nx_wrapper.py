f5381e85a05d68ad740aa484e8cf323bab4dd301
SkBlaz <skrljblaz@gmail.com>
2018-09-01 09:03:57 +0200

    centrality example

---
+++ examples/example_nx_wrapper.py
@@ -0,0 +1,11 @@
+## a simple example for wrapping entworkx functions
+
+from py3plex.core import multinet
+from py3plex.core import random_generators
+
+multilayer_network = random_generators.random_multilayer_ER(300,6,0.05,directed=False)
+
+## treat as monoplex network
+centralities = multilayer_network.monoplex_nx_wrapper("betweenness_centrality")
+print(centralities)
+
