11973d23dcc616161bb76850e594f248eb2b20f9
skblaz <skrljblaz@gmail.com>
2018-11-07 12:47:24 +0100

    save and load example added.

---
+++ examples/example_save_to_gpickle.py
@@ -0,0 +1,19 @@
+## simple example of how a network can be easily saved!
+from py3plex.core import multinet
+
+dataset = "../datasets/imdb_gml.gml"
+
+## load as GML
+multilayer_network = multinet.multi_layer_network().load_network(input_file=dataset,directed=True,input_type=dataset.split(".")[-1])
+
+## save to gpickle
+multilayer_network.save_network("../datasets/imdb.gpickle",output_type="gpickle")
+multilayer_network_new = multinet.multi_layer_network()
+multilayer_network_new.load_network("../datasets/imdb.gpickle",input_type="gpickle")
+
+## show some very basic stats
+multilayer_network_new.basic_stats()
+
+triplet_set = list(set(multilayer_network.get_decomposition_cycles()))
+print(triplet_set)
+
