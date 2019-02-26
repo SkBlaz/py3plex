8155570fbe1be39ddd4ddc6cbb407a2234541cd9
jan <jan.kralj@ijs.si>
2019-02-26 09:54:14 +0100

    cleaned up the formulas for midpoint calculation

--- py3plex/visualization/bezier.py
+++ py3plex/visualization/bezier.py
@@ -4,64 +4,65 @@
 import numpy as np # this is used for vectorized bezier computation
 from scipy.interpolate import CubicSpline
 
+
 def draw_bezier(total_size,p1,p2,mode="quadratic",inversion=False,path_height=2, linemode="both",resolution=0.1):
     if mode == "quadratic":
 
         if p1[0] < p1[1]:
-            x0,x1 = p1
-            y0,y1 = p2
+            x0, x1 = p1
+            y0, y1 = p2
         else:
-            x1,x0 = p1
-            y1,y0 = p2            
+            x1, x0 = p1
+            y1, y0 = p2
 
         ## coordinate init phase
-        dfx = np.arange(x0,x1,resolution)                
+        dfx = np.arange(x0, x1, resolution)
         slope = (y1-y0)/(x1-x0)
-        n = y1-slope*x1
-        midpoint_x = (x0+x1)/2
-        mp_y = slope*midpoint_x+n
-        nn = mp_y-midpoint_x*(-1/slope)
-        
+        midpoint_x = (x0+x1) / 2
+        mp_y = (y0 + y1) / 2
+
+        midpoint_y = None
         ## x0 x1, y0 y1                
         if linemode == "both":
 
-            r1 = np.round(y0,0)
-            r2 = np.round(y1,0)
+            r1 = np.round(y0, 0)
+            r2 = np.round(y1, 0)
             try:                        
                 if r1 > y0 and r2 > y1:
-                    midpoint_y = (midpoint_x*(-1/slope)+nn)*path_height
-                    x_t = [x0,midpoint_x,x1]
-                    y_t = [y0,midpoint_y,y1]    
-                    cs = CubicSpline(x_t,y_t)
+                    midpoint_y = mp_y*path_height
+                    x_t = [x0, midpoint_x, x1]
+                    y_t = [y0, midpoint_y, y1]
+                    cs = CubicSpline(x_t, y_t)
                     dfy = cs(dfx)        
                 else:
-                    midpoint_y = 1/((midpoint_x*(-1/slope)+nn)*path_height)
-                    x_t = [x0,midpoint_x,x1]
-                    y_t = [y0,midpoint_y,y1]    
-                    cs = CubicSpline(x_t,y_t)
+                    midpoint_y = 1/(mp_y*path_height)
+                    x_t = [x0, midpoint_x, x1]
+                    y_t = [y0, midpoint_y, y1]
+                    cs = CubicSpline(x_t, y_t)
                     dfy = cs(dfx)
-            except:
-                print([x0,midpoint_x,x1],[y0,midpoint_y,y1])
+            except Exception as err:
+                #TODO: takle exception catching ni ok za koncno verzijo...
+                print([x0, midpoint_x, x1], [y0, midpoint_y, y1])
 
         elif linemode == "upper":
             try:
-                midpoint_y = (midpoint_x*(-1/slope)+nn)*path_height
-                x_t = [x0,midpoint_x,x1]
-                y_t = [y0,midpoint_y,y1]    
-                cs = CubicSpline(x_t,y_t)
+                midpoint_y = mp_y*path_height
+                x_t = [x0, midpoint_x, x1]
+                y_t = [y0, midpoint_y, y1]
+                cs = CubicSpline(x_t, y_t)
                 dfy = cs(dfx)        
             except Exception as err:
-                print([x0,midpoint_x,x1],[y0,midpoint_y,y1])
+                print([x0, midpoint_x, x1],[y0,midpoint_y,y1])
             
         elif linemode == "bottom":
             try:
-                midpoint_y = 1/((midpoint_x*(-1/slope)+nn)*path_height)
-                x_t = [x0,midpoint_x,x1]
-                y_t = [y0,midpoint_y,y1]    
-                cs = CubicSpline(x_t,y_t)
+                midpoint_y = 1/(mp_y*path_height)
+                x_t = [x0, midpoint_x, x1]
+                y_t = [y0, midpoint_y, y1]
+                cs = CubicSpline(x_t, y_t)
                 dfy = cs(dfx)
             except Exception as err:
-                print([x0,midpoint_x,x1],[y0,midpoint_y,y1])
+                print([x0, midpoint_x, x1], [y0, midpoint_y, y1])
 
         return (dfx,dfy)
     
